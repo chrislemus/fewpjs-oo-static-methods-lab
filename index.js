417b090921b10cb60a02528f64feb6271ecd854b
chrislemus <icrislemus@gmail.com>
2021-03-18 07:55:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.substring(1)
+  }
+  static sanitize(string) {
+    const filteredWords = string.match(/[\w\d\s-']/g)
+    return filteredWords ? filteredWords.join('') : ''
+  }
+  static titleize(string) {
+    const noCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    return string.split(/\s/).map((word, idx) => {
+      const isCap = idx === 0 || !noCap.includes(word)
+      return isCap ? this.capitalize(word) : word
+    }).join(' ')
+  }
+  
 }
